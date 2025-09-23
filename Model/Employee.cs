ea3fe877568c2adb8a6053a7c873b6e78867b712
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-09-23 09:45:26 +0530

    Create Employee.cs

---
+++ Model/Employee.cs
@@ -0,0 +1,12 @@
+namespace Api_First.Model
+{
+    public class Employee
+    {
+
+        public string Name {  get; set; }
+        public string EmpId { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+        public string Desgination { get; set; }
+    }
+}
